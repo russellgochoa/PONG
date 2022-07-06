ffd8b46a6f469f71907e335a027292a0f438c3ed
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-06 15:54:46 -0500

    game.html and pong.css grid updates

--- pong.js
+++ pong.js
@@ -1,9 +1,32 @@
+////////////////////////////////
+// Global Variables Here
+// need a score variable
+
 let score = 0
-const playerOnePaddle = document.getElementsByClassName('.paddle_1')
-const computerPaddle = document.getElementsByClassName('.computer_paddle')
-const ball = document.getElementsByClassName('.ball')
+const dot = document.getElementsByClassName('.ball')
+
+////////////////////////////////
+// Functions For Game Logic Here
+
+function moveDot()
+
+// select divs from html file
+
+// make a dot
+
+// function that makes dot appear in random location on screen
+// make css grid "container" w/ 9 cells
+// each cell is it's own div
+// populate divs one at a time with the dot
+
+////////////////////////////////
+// Event Listeners Here
+
+// listen for click event on the dot
+// when the click event happens, we need to fire a function
+// function that fires needs to increase score and needs to invoke the function that moves the dot
+
 
 // for (let i = 0; i < ball.length; i++) {
-ball.addEventListener('click', () => {
-  console.log('did this work?')
-})
+
+
