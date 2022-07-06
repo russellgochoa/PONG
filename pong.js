d799a56424fcd403a46e20a3e4093a4028f6550e
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-06 18:31:20 -0500

    added pong.css variables

--- pong.js
+++ pong.js
@@ -3,13 +3,20 @@
 // need a score variable
 
 let score = 0
-const dot = document.getElementsByClassName('.ball')
-
+let dotSquare = 0
+const dot = document.getElementsByClassName('dot')
+const squares = document.getElementsByClassName('square')
+// console.log(squares)
 ////////////////////////////////
 // Functions For Game Logic Here
 
-function moveDot()
-
+//function moveDot()
+for (i = 0; i < squares.length; i++) {
+  if (squares[i].innerHTML === '<div class="dot"></div>') {
+    dotSquare = i
+  }
+}
+console.log(dotSquare)
 // select divs from html file
 
 // make a dot
@@ -21,12 +28,7 @@ function moveDot()
 
 ////////////////////////////////
 // Event Listeners Here
-
+// .addEventListener('click', () =>
 // listen for click event on the dot
 // when the click event happens, we need to fire a function
 // function that fires needs to increase score and needs to invoke the function that moves the dot
-
-
-// for (let i = 0; i < ball.length; i++) {
-
-
