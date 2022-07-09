bd5a4bdd28a9e82471c9a46e231f1a8d7a1c9103
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-08 19:41:23 -0500

    Math.random experiment

--- game.js
+++ game.js
@@ -8,5 +8,25 @@ for (let i = 0; i < squares.length; i++) {
     if (event.target.children.length > 0) {
       squares[i].innerText = ''
     }
+    //console.log((squares[i].dotSquare = Math.ceil(Math.random() * 9)))
   })
 }
+
+// each cell is it's own div
+// populate divs one at a time with the dot
+
+// when the click event happens, we need to fire a function
+// function that fires needs to increase score and needs to invoke the function that moves the dot
+
+// moveDot()??
+
+// function moveDot(event) {}
+// dot.addEventListener('click', (event) => {
+//   if (event.target.dot) ???
+// })
+
+// function that makes dot appear in random location on screen
+
+// function randomSquare(min, max) {
+// console.log(Math.ceil(Math.random() * 9))
+// } ???
